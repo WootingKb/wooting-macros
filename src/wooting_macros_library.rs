0f5f5bb681148e5008bb9f44eeec26d1607e8fee
medzernik <medzernik1@gmail.com>
2022-10-16 16:34:58 +0200

    working on the structure

--- src/wooting_macros_library.rs
+++ src/wooting_macros_library.rs
@@ -24,8 +24,16 @@ pub struct Key {
     pub press_duration: time::Duration,
 }
 
+///This basically serves as the implementation that gets called from the methods on KeyPress enum.
+/// We always handle the input as: Keypress comes in -> triage -> call the execute according to logic -> process.
+// TODO: This poses a question: do we change the enum variants to just a struct field instead of the enum embodying the struct?
 impl Key {
+    fn execute_key_up(&self){
 
+    }
+    fn execute_key_down(&self){
+
+    }
 }
 
 ///KeyPress wraps the type of a keypress (Action, KeyUp, KeyDown). For parsing the output (using it).
@@ -48,9 +56,55 @@ pub enum KeyPress {
     UnicodeDirect(),
 }
 
+/*
+trait Execute{
+    fn execute (&self) {
+
+    }
+}
+
+
+
+
+impl Execute for KeyPress {
+    fn execute(&self) {
+    }
+}
+
+ */
+
 impl KeyPress {
 
+    //TODO: execute function? Seems a bit annoying but could work.
+    //TODO: Make a trait probably, that has an "execute" function, implemented indivudally. Special logic for the KeyUp and Down linking.
+    fn press_key_down(&self){
+        match self {
+            KeyPress::KeyDownEvent(s) =>{
+                s.execute_key_down();
+            },
+            KeyPress::KeyUpEvent(s) => {
+                s.execute_key_up();
+            },
+            KeyPress::SystemEvent(s) => {
+                todo!("Not yet done");
+            },
+            KeyPress::PhillipsHueCommand() => {
+                todo!("Not yet done");
+            },
+            KeyPress::DiscordCommand() => {
+                todo!("Not yet done");
+            },
+            KeyPress::UnicodeDirect() => {
+                todo!("Not yet done");
+            },
+            _ => unimplemented!("Error! Implement this!"),
+
+        }
+
+    }
 }
+
+
 
 #[derive(Debug)]
 pub struct Action {
@@ -87,7 +141,7 @@ impl MacroGroup {
 }
 
 pub fn run_this() {
-    let testing_keypress = KeyPress::KeyUpEvent(Key {
+    let testing_keypress = KeyPress::KeyDownEvent(Key {
         keypress: 'c',
         press_wait_delay_after: time::Duration::from_millis(50),
         press_duration: time::Duration::from_millis(50),
@@ -97,6 +151,8 @@ pub fn run_this() {
         action: 'd',
         press_wait_delay_after: time::Duration::from_millis(5),
     });
+
+
 
     match testing_keypress {
         KeyPress::KeyUpEvent(s) => println!("Result: {}", s.keypress),
