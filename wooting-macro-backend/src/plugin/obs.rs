08a8ae2afa855060c5cc961476ec997bdf433d0d
medzernik <medzernik1@gmail.com>
2022-12-30 12:40:05 +0100

    initial connection can be estabilished now

--- wooting-macro-backend/src/plugin/obs.rs
+++ wooting-macro-backend/src/plugin/obs.rs
@@ -1,18 +1,21 @@
 use log::*;
 use obws::Client;
 
+
 /// Engage the OBS plugin. Not done.
 pub async fn engage_obs() {
     // Connect to the OBS instance through obs-websocket.
-    let client = Client::connect("localhost", 4455, Some("password"))
+    let client = Client::connect("localhost", 4455, Some("123456"))
         .await
         .unwrap();
 
     // Get and print out version information of OBS and obs-websocket.
     let version = client.general().version().await.unwrap();
     info!("{:#?}", version);
+    println!("{:#?}", version);
 
     // Get a list of available scenes and print them out.
     let scene_list = client.scenes().list().await.unwrap();
     info!("{:#?}", scene_list);
+    println!("{:#?}", scene_list);
 }
