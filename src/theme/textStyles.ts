789cd1a9e96d3920e9d5bc9074e90e2a405ad0fc
Khang <khang.nguyen5700@gmail.com>
2023-01-04 18:38:29 -0700

    Update textStyles.ts

--- src/theme/textStyles.ts
+++ src/theme/textStyles.ts
@@ -11,7 +11,7 @@ export const textStyles = {
     fontFamily: 'Montserrat',
     fontWeight: 'extraBold',
     fontSize: 32,
-    textColor: 'primary-light.400',
-    '.chakra-ui-dark &': { textColor: 'primary-dark.400' }
+    textColor: 'primary-light.900',
+    '.chakra-ui-dark &': { textColor: 'primary-dark.100' }
   },
 }
