826791762585868c5220e85c6d276c2a40c84a31
medzernik <medzernik1@gmail.com>
2023-01-05 17:06:57 +0100

    working on async benches

--- wooting-macro-backend/benches/file_creation_benchmark.rs
+++ wooting-macro-backend/benches/file_creation_benchmark.rs
@@ -1,4 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::async_executor::AsyncExecutor;
+
+use wooting_macro_backend::*;
+
 
 fn fibonacci(n: u64) -> u64 {
     match n {
@@ -6,11 +10,19 @@ fn fibonacci(n: u64) -> u64 {
         1 => 1,
         n => fibonacci(n-1) + fibonacci(n-2),
     }
+
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
 }
 
-criterion_group!(benches, criterion_benchmark);
+
+fn data_creation_time (c: &mut Criterion){
+    let test = wooting_macro_backend::MacroBackend::default();
+
+    c.bench_function("backend create", |b| b.iter(||test.init().await));
+}
+
+criterion_group!(benches, data_creation_time);
 criterion_main!(benches);
