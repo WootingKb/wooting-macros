85ce1739a3ed193a8c1a896e23b8c72189f6e02a
medzernik <medzernik1@gmail.com>
2023-01-06 10:56:44 +0100

    first benchmark working?

--- wooting-macro-backend/benches/file_creation_benchmark.rs
+++ wooting-macro-backend/benches/file_creation_benchmark.rs
@@ -1,27 +1,31 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+
 use criterion::async_executor::AsyncExecutor;
+
+use std::time::Duration;
+
 
 use wooting_macro_backend::*;
 
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
-    }
-
-}
-
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
-}
+// fn fibonacci(n: u64) -> u64 {
+//     match n {
+//         0 => 1,
+//         1 => 1,
+//         n => fibonacci(n-1) + fibonacci(n-2),
+//     }
+//
+// }
+//
+// fn criterion_benchmark(c: &mut Criterion) {
+//     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+// }
 
 
 fn data_creation_time (c: &mut Criterion){
-    let test = wooting_macro_backend::MacroBackend::default();
+    //c.measurement_time(Duration::from_secs(30));
 
-    c.bench_function("backend create", |b| b.iter(||test.init().await));
+    c.bench_function("backend create", |b| b.iter(||MacroBackend::default()));
 }
 
 criterion_group!(benches, data_creation_time);
