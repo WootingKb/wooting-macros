d96b16f0fc52fa1e924ff4ebe0d83c14f037c309
medzernik <1900179+medzernik@users.noreply.github.com>
2023-02-07 17:31:47 +0100

    println log fix

--- wooting-macro-backend/src/bin/main.rs
+++ wooting-macro-backend/src/bin/main.rs
@@ -1,12 +1,14 @@
 use wooting_macro_backend::MacroBackend;
+use log::*;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 /// This main function serves only for running the backend. Backend can be run without frontend parts as most things are separated.
 async fn main() {
-    println!("Running only backend");
-    let backend = MacroBackend::default();
-
     env_logger::init();
+    
+    info!("Running only backend");
+
+    let backend = MacroBackend::default();
 
     backend.init().await;
 }
