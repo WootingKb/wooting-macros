3280008c4b561f150bbd8501b1a2798f44410dd1
medzernik <1900179+medzernik@users.noreply.github.com>
2023-02-08 12:47:31 +0100

    added default log to backend only

--- wooting-macro-backend/src/bin/main.rs
+++ wooting-macro-backend/src/bin/main.rs
@@ -1,10 +1,13 @@
+use env_logger::Env;
 use wooting_macro_backend::MacroBackend;
 use log::*;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 /// This main function serves only for running the backend. Backend can be run without frontend parts as most things are separated.
 async fn main() {
-    env_logger::init();
+    
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     
     info!("Running only backend");
 
