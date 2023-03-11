18896adb94713071d4d6ae789412608a7abf784a
Aron-Kristofori <91370181+Aron-Kristofori@users.noreply.github.com>
2023-03-11 12:12:42 +0100

    Removed unneccessary delay and tested

--- wooting-macro-backend/src/lib.rs
+++ wooting-macro-backend/src/lib.rs
@@ -282,14 +282,6 @@ async fn execute_macro(macros: Macro, channel: UnboundedSender<rdev::EventType>)
 fn keypress_executor_sender(mut rchan_execute: UnboundedReceiver<rdev::EventType>) {
     loop {
         plugin::util::send(&rchan_execute.blocking_recv().unwrap());
-
-        //Windows requires a delay between each macro execution.
-        #[cfg(any(target_os = "macos", target_os = "linux"))]
-        thread::sleep(time::Duration::from_millis(10));
-
-        //MacOS requires some strange delays so putting it here just in case.
-        #[cfg(target_os = "windows")]
-        thread::sleep(time::Duration::from_millis(1));
     }
 }
 
