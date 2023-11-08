a00096f1e07e4f63d71957bc460b4b3135d36b7b
medzernik <1900179+medzernik@users.noreply.github.com>
2023-11-08 12:43:19 +0100

    Updated F key and media key definitions

--- wooting-macro-backend/src/hid_table.rs
+++ wooting-macro-backend/src/hid_table.rs
@@ -25,7 +25,6 @@ pub static ref SCANCODE_TO_RDEV: HashMap<u32, Key> = {
         scancode.insert(0x05, Key::KeyB); //US_B
         scancode.insert(0x06, Key::KeyC); //US_C
         scancode.insert(0x07, Key::KeyD); //US_D
-
         scancode.insert(0x08, Key::KeyE); //US_E
         scancode.insert(0x09, Key::KeyF); //US_F
         scancode.insert(0x0a, Key::KeyG); //US_G
@@ -34,7 +33,6 @@ pub static ref SCANCODE_TO_RDEV: HashMap<u32, Key> = {
         scancode.insert(0x0d, Key::KeyJ); //US_J
         scancode.insert(0x0e, Key::KeyK); //US_K
         scancode.insert(0x0f, Key::KeyL); //US_L
-
         scancode.insert(0x10, Key::KeyM); //US_M
         scancode.insert(0x11, Key::KeyN); //US_N
         scancode.insert(0x12, Key::KeyO); //US_O
@@ -43,16 +41,15 @@ pub static ref SCANCODE_TO_RDEV: HashMap<u32, Key> = {
         scancode.insert(0x15, Key::KeyR); //US_R
         scancode.insert(0x16, Key::KeyS); //US_S
         scancode.insert(0x17, Key::KeyT); //US_T
-
         scancode.insert(0x18, Key::KeyU); //US_U
         scancode.insert(0x19, Key::KeyV); //US_V
         scancode.insert(0x1a, Key::KeyW); //US_W
         scancode.insert(0x1b, Key::KeyX); //US_X
         scancode.insert(0x1c, Key::KeyY); //US_Y
         scancode.insert(0x1d, Key::KeyZ); //US_Z
+
         scancode.insert(0x1e, Key::Num1); //DIGIT1
         scancode.insert(0x1f, Key::Num2); //DIGIT2
-
         scancode.insert(0x20, Key::Num3); //DIGIT3
         scancode.insert(0x21, Key::Num4); //DIGIT4
         scancode.insert(0x22, Key::Num5); //DIGIT5
@@ -81,9 +78,9 @@ pub static ref SCANCODE_TO_RDEV: HashMap<u32, Key> = {
         scancode.insert(0x35, Key::BackQuote); //BACKQUOTE
         scancode.insert(0x36, Key::Comma); //COMMA
         scancode.insert(0x37, Key::Dot); //PERIOD
-
         scancode.insert(0x38, Key::Slash); //SLASH
         scancode.insert(0x39, Key::CapsLock); //CAPS_LOCK
+
         scancode.insert(0x3a, Key::F1); //F1
         scancode.insert(0x3b, Key::F2); //F2
         scancode.insert(0x3c, Key::F3); //F3
@@ -97,51 +94,22 @@ pub static ref SCANCODE_TO_RDEV: HashMap<u32, Key> = {
         scancode.insert(0x43, Key::F10); //F10
         scancode.insert(0x44, Key::F11); //F11
         scancode.insert(0x45, Key::F12); //F12
+        scancode.insert(0x68, Key::F13); //F13
+        scancode.insert(0x69, Key::F14); //F14
+        scancode.insert(0x6a, Key::F15); //F15
+        scancode.insert(0x6b, Key::F16); //F16
+        scancode.insert(0x6c, Key::F17); //F17
+        scancode.insert(0x6d, Key::F18); //F18
+        scancode.insert(0x6e, Key::F19); //F19
+        scancode.insert(0x6f, Key::F20); //F20
+        scancode.insert(0x70, Key::F21); //F21
+        scancode.insert(0x71, Key::F22); //F22
+        scancode.insert(0x72, Key::F23); //F23
+        scancode.insert(0x73, Key::F24); //F24
 
-
-        #[cfg(target_os = "windows")]
-        {
-        scancode.insert(0x7f, Key::Unknown(173)); //VOLUME_MUTE
-        scancode.insert(0x81, Key::Unknown(174)); //VOLUME_DOWN
-        scancode.insert(0x80, Key::Unknown(175)); //VOLUME_UP
-
-        scancode.insert(0x68, Key::Unknown(124)); //F13
-        scancode.insert(0x69, Key::Unknown(125)); //F14
-        scancode.insert(0x6a, Key::Unknown(126)); //F15
-        scancode.insert(0x6b, Key::Unknown(127)); //F16
-
-        scancode.insert(0x6c, Key::Unknown(128)); //F17
-        scancode.insert(0x6d, Key::Unknown(129)); //F18
-        scancode.insert(0x6e, Key::Unknown(130)); //F19
-        scancode.insert(0x6f, Key::Unknown(131)); //F20
-
-        scancode.insert(0x70, Key::Unknown(132)); //F21
-        scancode.insert(0x71, Key::Unknown(133)); //F22
-        scancode.insert(0x72, Key::Unknown(134)); //F23
-        scancode.insert(0x73, Key::Unknown(135)); //F24
-        }
-
-        #[cfg(target_os = "linux")]
-        {
-        scancode.insert(0x7f, Key::Unknown(121)); //VOLUME_MUTE
-        scancode.insert(0x81, Key::Unknown(122)); //VOLUME_DOWN
-        scancode.insert(0x80, Key::Unknown(123)); //VOLUME_UP
-
-        scancode.insert(0x68, Key::Unknown(191)); //F13
-        scancode.insert(0x69, Key::Unknown(192)); //F14
-        scancode.insert(0x6a, Key::Unknown(193)); //F15
-        scancode.insert(0x6b, Key::Unknown(194)); //F16
-
-        scancode.insert(0x6c, Key::Unknown(195)); //F17
-        scancode.insert(0x6d, Key::Unknown(196)); //F18
-        scancode.insert(0x6e, Key::Unknown(197)); //F19
-        scancode.insert(0x6f, Key::Unknown(198)); //F20
-
-        scancode.insert(0x70, Key::Unknown(199)); //F21
-        scancode.insert(0x71, Key::Unknown(200)); //F22
-        scancode.insert(0x72, Key::Unknown(201)); //F23
-        scancode.insert(0x73, Key::Unknown(202)); //F24
-        }
+        scancode.insert(0x7f, Key::VolumeMute); //VOLUME_MUTE
+        scancode.insert(0x81, Key::VolumeDown); //VOLUME_DOWN
+        scancode.insert(0x80, Key::VolumeUp); //VOLUME_UP
 
         scancode.insert(0x46, Key::PrintScreen); //PRINT_SCREEN
         scancode.insert(0x47, Key::ScrollLock); //SCROLL_LOCK
@@ -232,7 +200,6 @@ pub static ref SCANCODE_TO_HID: HashMap<Key, u32> = {
         scancode.insert(Key::KeyB, 0x05); //US_B
         scancode.insert(Key::KeyC, 0x06); //US_C
         scancode.insert(Key::KeyD, 0x07); //US_D
-
         scancode.insert(Key::KeyE, 0x08); //US_E
         scancode.insert(Key::KeyF, 0x09); //US_F
         scancode.insert(Key::KeyG, 0x0a); //US_G
@@ -241,7 +208,6 @@ pub static ref SCANCODE_TO_HID: HashMap<Key, u32> = {
         scancode.insert(Key::KeyJ, 0x0d); //US_J
         scancode.insert(Key::KeyK, 0x0e); //US_K
         scancode.insert(Key::KeyL, 0x0f); //US_L
-
         scancode.insert(Key::KeyM, 0x10); //US_M
         scancode.insert(Key::KeyN, 0x11); //US_N
         scancode.insert(Key::KeyO, 0x12); //US_O
@@ -250,16 +216,15 @@ pub static ref SCANCODE_TO_HID: HashMap<Key, u32> = {
         scancode.insert(Key::KeyR, 0x15); //US_R
         scancode.insert(Key::KeyS, 0x16); //US_S
         scancode.insert(Key::KeyT, 0x17); //US_T
-
         scancode.insert(Key::KeyU, 0x18); //US_U
         scancode.insert(Key::KeyV, 0x19); //US_V
         scancode.insert(Key::KeyW, 0x1a); //US_W
         scancode.insert(Key::KeyX, 0x1b); //US_X
         scancode.insert(Key::KeyY, 0x1c); //US_Y
         scancode.insert(Key::KeyZ, 0x1d); //US_Z
+
         scancode.insert(Key::Num1, 0x1e); //DIGIT1
         scancode.insert(Key::Num2, 0x1f); //DIGIT2
-
         scancode.insert(Key::Num3, 0x20); //DIGIT3
         scancode.insert(Key::Num4, 0x21); //DIGIT4
         scancode.insert(Key::Num5, 0x22); //DIGIT5
@@ -288,66 +253,37 @@ pub static ref SCANCODE_TO_HID: HashMap<Key, u32> = {
         scancode.insert(Key::BackQuote, 0x35); //BACKQUOTE
         scancode.insert(Key::Comma, 0x36); //COMMA
         scancode.insert(Key::Dot, 0x37); //PERIOD
-
         scancode.insert( Key::Slash, 0x38); //SLASH
         scancode.insert( Key::CapsLock, 0x39); //CAPS_LOCK
+
         scancode.insert( Key::F1, 0x3a); //F1
         scancode.insert( Key::F2, 0x3b); //F2
         scancode.insert( Key::F3, 0x3c); //F3
         scancode.insert( Key::F4, 0x3d); //F4
         scancode.insert( Key::F5, 0x3e); //F5
         scancode.insert( Key::F6, 0x3f); //F6
-
         scancode.insert(Key::F7, 0x40); //F7
         scancode.insert(Key::F8, 0x41); //F8
         scancode.insert(Key::F9, 0x42); //F9
         scancode.insert(Key::F10, 0x43); //F10
         scancode.insert(Key::F11, 0x44); //F11
         scancode.insert(Key::F12, 0x45); //F12
+        scancode.insert(Key::F13, 0x68); //F13
+        scancode.insert(Key::F14, 0x69); //F14
+        scancode.insert(Key::F15, 0x6a); //F15
+        scancode.insert(Key::F16, 0x6b); //F16
+        scancode.insert(Key::F17, 0x6c); //F17
+        scancode.insert(Key::F18, 0x6d); //F18
+        scancode.insert(Key::F19, 0x6e); //F19
+        scancode.insert(Key::F20, 0x6f); //F20
+        scancode.insert(Key::F21, 0x70); //F21
+        scancode.insert(Key::F22, 0x71); //F22
+        scancode.insert(Key::F23, 0x72); //F23
+        scancode.insert(Key::F24, 0x73); //F24
 
-        #[cfg(target_os = "windows")]
-        {
-        scancode.insert(Key::Unknown(173), 0x7f); //VOLUME_MUTE
-        scancode.insert(Key::Unknown(174), 0x81); //VOLUME_DOWN
-        scancode.insert(Key::Unknown(175), 0x80); //VOLUME_UP
-
-        scancode.insert(Key::Unknown(124), 0x68); //F13
-        scancode.insert(Key::Unknown(125), 0x69); //F14
-        scancode.insert(Key::Unknown(126), 0x6a); //F15
-        scancode.insert(Key::Unknown(127), 0x6b); //F16
-
-        scancode.insert(Key::Unknown(128), 0x6c); //F17
-        scancode.insert(Key::Unknown(129), 0x6d); //F18
-        scancode.insert(Key::Unknown(130), 0x6e); //F19
-        scancode.insert(Key::Unknown(131), 0x6f); //F20
-
-        scancode.insert(Key::Unknown(132), 0x70); //F21
-        scancode.insert(Key::Unknown(133), 0x71); //F22
-        scancode.insert(Key::Unknown(134), 0x72); //F23
-        scancode.insert(Key::Unknown(135), 0x73); //F24
-        }
-
-        #[cfg(target_os = "linux")]
-        {
-        scancode.insert(Key::Unknown(121), 0x7f); //VOLUME_MUTE
-        scancode.insert(Key::Unknown(122), 0x81); //VOLUME_DOWN
-        scancode.insert(Key::Unknown(123), 0x80); //VOLUME_UP
-
-        scancode.insert(Key::Unknown(191), 0x68); //F13
-        scancode.insert(Key::Unknown(192), 0x69); //F14
-        scancode.insert(Key::Unknown(193), 0x6a); //F15
-        scancode.insert(Key::Unknown(194), 0x6b); //F16
-
-        scancode.insert(Key::Unknown(195), 0x6c); //F17
-        scancode.insert(Key::Unknown(196), 0x6d); //F18
-        scancode.insert(Key::Unknown(197), 0x6e); //F19
-        scancode.insert(Key::Unknown(198), 0x6f); //F20
-
-        scancode.insert(Key::Unknown(199), 0x70); //F21
-        scancode.insert(Key::Unknown(200), 0x71); //F22
-        scancode.insert(Key::Unknown(201), 0x72); //F23
-        scancode.insert(Key::Unknown(202), 0x73); //F24
-        }
+        scancode.insert(Key::VolumeMute, 0x7f); //VOLUME_MUTE
+        scancode.insert(Key::VolumeDown, 0x81); //VOLUME_DOWN
+        scancode.insert(Key::VolumeUp, 0x80); //VOLUME_UP
 
         scancode.insert(Key::PrintScreen, 0x46); //PRINT_SCREEN
         scancode.insert(Key::ScrollLock, 0x47); //SCROLL_LOCK
