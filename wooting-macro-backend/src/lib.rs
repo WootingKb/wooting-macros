1095bee7fcbb805d22799e5f7d860426248e35e9
medzernik <1900179+medzernik@users.noreply.github.com>
2023-11-09 12:39:57 +0100

    Revert "Make mouse 1 always work, never grab it"
    This reverts commit f9b4c7e5cbf92473ddb6e63e4fa00deadab7ae0b.

--- wooting-macro-backend/src/lib.rs
+++ wooting-macro-backend/src/lib.rs
@@ -552,11 +552,10 @@ impl MacroBackend {
                                 channel_clone,
                             );
 
-                            // Left mouse button never gets consumed to allow users to control their PC.
-                            match (should_grab, button) {
-                                (true, rdev::Button::Left) => Some(event),
-                                (true, _) => None,
-                                (false, _) => Some(event),
+                            if should_grab {
+                                None
+                            } else {
+                                Some(event)
                             }
                         }
                         rdev::EventType::ButtonRelease(button) => {
