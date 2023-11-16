39f130961a2f1c2c8f7df1ae37250e6203a6f434
medzernik <1900179+medzernik@users.noreply.github.com>
2023-11-16 12:38:36 +0100

    Type renames

--- src/components/macroview/rightPanel/editForms/KeyPressForm.tsx
+++ src/components/macroview/rightPanel/editForms/KeyPressForm.tsx
@@ -90,7 +90,7 @@ export default function KeyPressForm({
 
     const temp: KeyPressEventAction = {
       ...selectedElement,
-      data: { ...selectedElement.data, press_duration: duration }
+      data: {...selectedElement.data, press_duration: duration}
     }
     updateElement(temp, selectedElementId)
   }, [
