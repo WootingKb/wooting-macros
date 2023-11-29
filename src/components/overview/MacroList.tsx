e58224ce46c3fd179ed60d34b69a3a67b104268d
medzernik <1900179+medzernik@users.noreply.github.com>
2023-11-29 13:24:05 +0100

    Added a search field for all macro name searching

--- src/components/overview/MacroList.tsx
+++ src/components/overview/MacroList.tsx
@@ -6,27 +6,52 @@ import {
   Grid,
   GridItem,
   useColorModeValue,
-  VStack
+  VStack,
 } from '@chakra-ui/react'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useApplicationContext } from '../../contexts/applicationContext'
 import { useSelectedCollection } from '../../contexts/selectors'
 import { ViewState } from '../../constants/enums'
-import { Macro } from '../../types'
+import { Collection, Macro } from '../../types'
 import MacroCard from './MacroCard'
 import useScrollbarStyles from '../../hooks/useScrollbarStyles'
 import useMainBgColour from '../../hooks/useMainBgColour'
+import { motion } from 'framer-motion'
+
 import { borderRadiusStandard } from "../../theme/config";
 
 export default function MacroList() {
-  const { selection, onCollectionUpdate, changeViewState } =
-    useApplicationContext()
+  // Get the ApplicationContext from your hook.
+  const {collections, selection, onCollectionUpdate, changeViewState, searchValue} =
+    useApplicationContext();
   const currentCollection = useSelectedCollection()
   const shadowColour = useColorModeValue('md', 'white-md')
+  const [matchingMacros, setMatchingMacros] = useState<{ macro: Macro; collection: Collection; }[]>([]);
+
+  useEffect(() => {
+    let newMatchingMacros: { macro: Macro; collection: Collection }[] = [];
+
+    if (searchValue) {
+      collections.map((collection) => {
+        collection.macros.map((macro) => {
+          if (macro.name.toLocaleLowerCase().includes(searchValue)) {
+            newMatchingMacros.push({macro, collection});
+          }
+        });
+      });
+      setMatchingMacros(newMatchingMacros);
+    } else {
+      setMatchingMacros(currentCollection.macros.map((macro, index) => ({
+        macro,
+        collection: currentCollection,
+        index
+      })));
+    }
+  }, [searchValue, collections, currentCollection]);
 
   const onMacroDelete = useCallback(
     (macroIndex: number) => {
-      const newCollection = { ...currentCollection }
+      const newCollection = {...currentCollection}
       newCollection.macros = newCollection.macros.filter(
         (_, i) => i !== macroIndex
       )
@@ -49,35 +74,65 @@ export default function MacroList() {
         templateColumns={['repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)']}
         gap={'25px'}
       >
-        <Flex h="full" justifyContent="center" alignItems="center">
-          <VStack
-            w="full"
-            minH="202px"
-            h="full"
-            bg={useMainBgColour()}
-            boxShadow={shadowColour}
-            rounded={borderRadiusStandard}
-            p={3}
-            m="auto"
-            justifyContent="center"
-            spacing={2}
-          >
-            <Button
-              variant="yellowGradient"
-              leftIcon={<AddIcon />}
-              size={['sm', 'md', 'lg']}
-              onClick={() => {
-                changeViewState(ViewState.Addview)
-              }}
-            >
-              Add Macro
-            </Button>
-          </VStack>
-        </Flex>
-        {currentCollection.macros.map((macro: Macro, index: number) => (
-          <GridItem w="full" h="fit-content" key={`${macro.name} + ${index}`}>
-            <MacroCard macro={macro} index={index} onDelete={onMacroDelete} />
-          </GridItem>
+        {!searchValue &&
+            <>
+                <motion.div
+
+                    initial={{x: '100vh'}}
+                    animate={{x: 0}}
+                    transition={{ease: "circOut", duration: 0.2}}
+                    exit={{x: '100vh'}}>
+                    <Flex h="full" justifyContent="center" alignItems="center">
+                        <VStack
+                            w="full"
+                            minH="202px"
+                            h="full"
+                            bg={useMainBgColour()}
+                            boxShadow={shadowColour}
+                            rounded="2xl"
+                            p={3}
+                            m="auto"
+                            justifyContent="center"
+                            spacing={2}
+                        >
+                            <Button
+                                variant="yellowGradient"
+                                leftIcon={<AddIcon/>}
+                                size={['sm', 'md', 'lg']}
+                                onClick={() => {
+                                  changeViewState(ViewState.Addview)
+                                }}
+                            >
+                                Add Macro
+                            </Button>
+                        </VStack>
+                    </Flex>
+                </motion.div>
+              {currentCollection.macros.map((macro, index) => (
+                <motion.div
+                  key={`${macro.name} + ${index}`}
+                  initial={{x: '100vh'}}
+                  transition={{ease: "circOut", duration: 0.2}}
+                  animate={{x: 0}}
+                  exit={{x: '100vh'}}>
+                  <GridItem w="full" h="fit-content">
+                    <MacroCard macro={macro} index={index} onDelete={onMacroDelete}/>
+                  </GridItem>
+                </motion.div>
+              ))}
+            </>
+        }
+        {searchValue && matchingMacros.map(({macro, collection}, index) => (
+          <motion.div
+            key={`${macro.name} + ${index}`}
+            initial={{x: '100vh'}}
+            transition={{ease: "circOut", duration: 0.2}}
+            animate={{x: 0}}
+            exit={{x: '100vh'}}>
+            <GridItem w="full" h="fit-content">
+              <MacroCard macro={macro} index={index} onDelete={onMacroDelete}/>
+            </GridItem>
+          </motion.div>
         ))}
       </Grid>
     </Box>
