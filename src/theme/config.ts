1fb763fbb5f0caddfc00f66dd5baf9526887a55c
medzernik <1900179+medzernik@users.noreply.github.com>
2023-12-12 13:03:49 +0100

    Border unification

--- src/theme/config.ts
+++ src/theme/config.ts
@@ -4,3 +4,7 @@ export const config: ThemeConfig = {
   initialColorMode: 'light',
   useSystemColorMode: false
 }
+
+export const borderRadiusStandard: { borderRadius: string } = {
+  borderRadius: '6px'
+}
