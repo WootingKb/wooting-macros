1bb43efa672a2c28cf3d1b24af8f6c4dd260b3cd
medzernik <1900179+medzernik@users.noreply.github.com>
2023-12-13 13:20:31 +0100

    Style changes

--- src/components/macrosettings/NotificationMacroSettingsPanel.tsx
+++ src/components/macrosettings/NotificationMacroSettingsPanel.tsx
@@ -1,9 +1,9 @@
-import { VStack } from "@chakra-ui/react";
+import { Text, VStack } from "@chakra-ui/react";
 
 export default function NotificationMacroSettingsPanel() {
   return (
-    <VStack w="full" spacing={4}>
-      <div>Coming Soon</div>
+    <VStack maxW="100px">
+      <Text>Coming Soon</Text>
     </VStack>
   )
 }
