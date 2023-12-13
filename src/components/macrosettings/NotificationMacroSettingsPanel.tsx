f4bcac66643d80564a67aa8a1c22ea04c13866a5
medzernik <1900179+medzernik@users.noreply.github.com>
2023-12-13 12:59:25 +0100

    Introduced advanced macro settings framework

---
+++ src/components/macrosettings/NotificationMacroSettingsPanel.tsx
@@ -0,0 +1,9 @@
+import { VStack } from "@chakra-ui/react";
+
+export default function NotificationMacroSettingsPanel() {
+  return (
+    <VStack w="full" spacing={4}>
+      <div>Coming Soon</div>
+    </VStack>
+  )
+}
--- src/components/macroview/centerPanel/sortableElement/SortableItem.tsx
+++ src/components/macroview/centerPanel/sortableElement/SortableItem.tsx
@@ -121,6 +121,7 @@ export default function SortableItem({
 
   return (
     <HStack
+
       w="full"
       h="full"
       bg={
