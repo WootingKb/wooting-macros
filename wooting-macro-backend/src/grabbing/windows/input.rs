0b08123a194a3c49058cc151735591c678221caa
medzernik <1900179+medzernik@users.noreply.github.com>
2024-01-16 13:31:26 +0100

    port of windows code back to rdev

--- wooting-macro-backend/src/grabbing/windows/input.rs
+++ wooting-macro-backend/src/grabbing/windows/input.rs
@@ -7,16 +7,91 @@ pub mod input {
     use itertools::Itertools;
     use log::*;
     use multiinput::*;
+    use rdev::EventType;
     use tokio::sync::mpsc::UnboundedSender;
 
     use crate::grabbing::executor::input::MacroExecutorEvent;
-    use crate::grabbing::windows::matcher::input::check_macro_execution_simply;
+    use crate::grabbing::windows::matcher::input::{
+        check_macro_execution_simply, check_macro_execution_simply_async,
+    };
     use crate::hid_table::*;
     use crate::macros::macro_data::MacroLookup;
     use crate::plugin::delay::DEFAULT_DELAY;
     use crate::RwLock;
 
     pub async fn check_keypress_simon(
+        inner_is_listening: Arc<AtomicBool>,
+        schan_macro_execute: UnboundedSender<MacroExecutorEvent>,
+        map: Arc<RwLock<MacroLookup>>,
+    ) {
+        tokio::time::sleep(time::Duration::from_millis(3000)).await;
+
+        let mut manager = RawInputManager::new().unwrap();
+        manager.register_devices(DeviceType::Keyboards);
+        warn!("Device list {:#?}", manager.get_device_list());
+
+        for device in manager.get_device_list().keyboards.iter() {
+            warn!("Device status {:#?}", device);
+        }
+
+        let mut og_previously_pressed_keys: Arc<RwLock<Vec<u32>>> = Arc::new(RwLock::from(vec![]));
+        let mut og_current_pressed_keys: Arc<RwLock<Vec<u32>>> = Arc::new(RwLock::from(vec![]));
+        let mut previously_pressed_buttons: Vec<u32> = vec![];
+        let mut current_pressed_buttons: Vec<u32> = vec![];
+
+        let _grabber = tokio::task::spawn_blocking(move || {
+            let schan_macro_execute_inner = schan_macro_execute.clone();
+            let mut current_pressed_keys = og_current_pressed_keys.clone();
+            let mut previously_pressed_keys = og_previously_pressed_keys.clone();
+
+            rdev::grab(move |event: rdev::Event| {
+                if inner_is_listening.load(Ordering::Relaxed) {
+                    match event.event_type {
+                        EventType::KeyPress(key) => {
+                            current_pressed_keys
+                                .blocking_write()
+                                .push(*RDEV_TO_HID.get(&key).unwrap_or_else(|| &0));
+
+                            // Need to keep only unique values
+                            let current_pressed_keys_clone =
+                                current_pressed_keys.blocking_read().clone();
+
+                            *current_pressed_keys.blocking_write() =
+                                current_pressed_keys_clone.into_iter().unique().collect();
+
+                            check_macro_execution_simply(
+                                &current_pressed_keys.blocking_read(),
+                                &previously_pressed_keys.blocking_read(),
+                                map.clone(),
+                                &schan_macro_execute_inner,
+                            );
+
+                            Some(event)
+                        }
+                        EventType::KeyRelease(key) => {
+                            *previously_pressed_keys.blocking_write() =
+                                current_pressed_keys.blocking_read().clone();
+
+                            current_pressed_keys
+                                .blocking_write()
+                                .retain(|x| x != RDEV_TO_HID.get(&key).unwrap_or_else(|| &0));
+
+                            Some(event)
+                        }
+                        EventType::ButtonPress(button) => Some(event),
+                        EventType::ButtonRelease(button) => Some(event),
+                        _ => Some(event),
+                    }
+                    // thread::sleep(time::Duration::from_millis(DEFAULT_DELAY));
+                } else {
+                    // thread::sleep(time::Duration::from_millis(2000));
+                    Some(event)
+                }
+            })
+        });
+    }
+
+    pub async fn check_keypress_simon_multiinput(
         inner_is_listening: Arc<AtomicBool>,
         schan_macro_execute: UnboundedSender<MacroExecutorEvent>,
         map: Arc<RwLock<MacroLookup>>,
@@ -45,7 +120,7 @@ pub mod input {
                                 current_pressed_keys =
                                     current_pressed_keys.into_iter().unique().collect();
 
-                                check_macro_execution_simply(
+                                check_macro_execution_simply_async(
                                     &current_pressed_keys,
                                     &previously_pressed_keys,
                                     map.clone(),
--- wooting-macro-backend/src/grabbing/windows/matcher.rs
+++ wooting-macro-backend/src/grabbing/windows/matcher.rs
@@ -11,7 +11,44 @@ pub mod input {
     use crate::macros::macro_data::MacroLookup;
     use crate::macros::macros::MacroType;
 
-    pub async fn check_macro_execution_simply(
+    pub fn check_macro_execution_simply(
+        current_pressed_keys: &Vec<u32>,
+        previously_pressed_keys: &Vec<u32>,
+        triggers: Arc<RwLock<MacroLookup>>,
+        schan_macro_execute: &UnboundedSender<MacroExecutorEvent>,
+    ) {
+        for (macro_id, macro_data) in triggers.blocking_read().id_map.iter() {
+            // Get the macro trigger
+            if let TriggerEventType::KeyPressEvent { data, .. } = &macro_data.config.trigger {
+                match (data, &current_pressed_keys, &previously_pressed_keys) {
+                    // If the keys are the same, skip checking
+                    (_trigger_combo, pressed, pressed_previous)
+                    if pressed == pressed_previous
+                        && macro_data.config.macro_type == MacroType::OnHold => {}
+                    // If the keys are different and its a trigger key pressed, start a macro
+                    (trigger_combo, pressed, _pressed_previous)
+                    if trigger_combo.iter().all(|x| pressed.contains(x)) =>
+                        {
+                            schan_macro_execute
+                                .send(MacroExecutorEvent::Start(macro_id.clone()))
+                                .unwrap();
+                        }
+                    // If the keys are different and its a trigger key released, stop a macro
+                    (trigger_combo, _pressed, pressed_previous)
+                    if trigger_combo.iter().all(|x| pressed_previous.contains(x)) =>
+                        {
+                            schan_macro_execute
+                                .send(MacroExecutorEvent::Stop(macro_id.clone()))
+                                .unwrap();
+                        }
+                    // Anything else just ignore
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    pub async fn check_macro_execution_simply_async(
         current_pressed_keys: &Vec<u32>,
         previously_pressed_keys: &Vec<u32>,
         triggers: Arc<RwLock<MacroLookup>>,
