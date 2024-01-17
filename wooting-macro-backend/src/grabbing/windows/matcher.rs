e9337997b26426df90c135cb206f7257ba20ad4f
medzernik <1900179+medzernik@users.noreply.github.com>
2024-01-17 12:51:12 +0100

    Basic mouse triggers now work

--- wooting-macro-backend/src/grabbing/windows/matcher.rs
+++ wooting-macro-backend/src/grabbing/windows/matcher.rs
@@ -7,9 +7,11 @@ pub mod input {
     use tokio::sync::RwLock;
 
     use crate::grabbing::executor::input::MacroExecutorEvent;
+    use crate::hid_table::BUTTON_TO_HID;
     use crate::macros::events::triggers::TriggerEventType;
     use crate::macros::macro_data::MacroLookup;
     use crate::macros::macros::MacroType;
+    use crate::plugin::mouse::MouseButton;
 
     pub fn check_macro_execution_simply(
         current_pressed_keys: &Vec<u32>,
@@ -20,37 +22,54 @@ pub mod input {
         let mut return_value = false;
         for (macro_id, macro_data) in triggers.blocking_read().id_map.iter() {
             // Get the macro trigger
-            if let TriggerEventType::KeyPressEvent { data, .. } = &macro_data.config.trigger {
-                match (data, &current_pressed_keys, &previously_pressed_keys) {
-                    // If the keys are the same, skip checking
-                    (_trigger_combo, pressed, pressed_previous)
-                        if pressed == pressed_previous
-                            && macro_data.config.macro_type == MacroType::OnHold => {
-                        return_value = true;
+            match &macro_data.config.trigger {
+                TriggerEventType::KeyPressEvent { data, .. } => {
+                    match (data, &current_pressed_keys, &previously_pressed_keys) {
+                        // If the keys are the same, skip checking
+                        (_trigger_combo, pressed, pressed_previous)
+                            if pressed == pressed_previous
+                                && macro_data.config.macro_type == MacroType::OnHold =>
+                        {
+                            return_value = true;
+                        }
+                        // If the keys are different and its a trigger key pressed, start a macro
+                        (trigger_combo, pressed, _pressed_previous)
+                            if trigger_combo.iter().all(|x| pressed.contains(x)) =>
+                        {
+                            schan_macro_execute
+                                .send(MacroExecutorEvent::Start(macro_id.clone()))
+                                .unwrap();
+                            return_value = true;
+                        }
+                        // If the keys are different and its a trigger key released, stop a macro
+                        (trigger_combo, _pressed, pressed_previous)
+                            if trigger_combo.iter().all(|x| pressed_previous.contains(x)) =>
+                        {
+                            schan_macro_execute
+                                .send(MacroExecutorEvent::Stop(macro_id.clone()))
+                                .unwrap();
+                            return_value = true;
+                        }
+                        // Anything else just ignore
+                        _ => {}
                     }
-                    // If the keys are different and its a trigger key pressed, start a macro
-                    (trigger_combo, pressed, _pressed_previous)
-                        if trigger_combo.iter().all(|x| pressed.contains(x)) =>
-                    {
-                        schan_macro_execute
-                            .send(MacroExecutorEvent::Start(macro_id.clone()))
-                            .unwrap();
-                        return_value = true;
+                }
+                TriggerEventType::MouseEvent { data } => {
+                    match (data, current_pressed_keys, previously_pressed_keys) {
+                        (trigger_combo, pressed, _pressed_previous)
+                            if pressed.iter().all(|x: &u32| u32::from(trigger_combo) == *x) =>
+                        {
+                            schan_macro_execute
+                                .send(MacroExecutorEvent::Start(macro_id.clone()))
+                                .unwrap();
+                            return_value = true;
+                        }
+                        _ => {}
                     }
-                    // If the keys are different and its a trigger key released, stop a macro
-                    (trigger_combo, _pressed, pressed_previous)
-                        if trigger_combo.iter().all(|x| pressed_previous.contains(x)) =>
-                    {
-                        schan_macro_execute
-                            .send(MacroExecutorEvent::Stop(macro_id.clone()))
-                            .unwrap();
-                        return_value = true;
-                    }
-                    // Anything else just ignore
-                    _ => {}
                 }
             }
         }
+
         return_value
     }
 
