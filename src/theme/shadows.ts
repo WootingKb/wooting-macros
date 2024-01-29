5830c56f2667220c804acb4b1dd463fcc54cf649
medzernik <1900179+medzernik@users.noreply.github.com>
2024-01-29 14:30:23 +0100

    Removed dead code

--- src/theme/shadows.ts
+++ src/theme/shadows.ts
@@ -1,8 +1,5 @@
 export const shadows = {
   xs: '0px 0px 4px 1px rgba(0, 0, 0, 0.16)',
-  'white-xs': '',
   sm: '0px 2px 4px rgba(0, 0, 0, 0.16)',
-  'white-sm': '',
   md: '0px 4px 8px rgba(0, 0, 0, 0.16)',
-  'white-md': ''
 }
