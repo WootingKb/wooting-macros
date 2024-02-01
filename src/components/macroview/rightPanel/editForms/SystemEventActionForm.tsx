1f7fa2fa3e2c674d965a85d3dfd5f231ec9e3119
medzernik <1900179+medzernik@users.noreply.github.com>
2024-02-01 14:02:12 +0100

    Removed ``useMemo`` that wasn't needed

--- src/components/macroview/rightPanel/editForms/SystemEventActionForm.tsx
+++ src/components/macroview/rightPanel/editForms/SystemEventActionForm.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react'
 import { SystemEventAction } from '../../../../types'
 import ClipboardForm from './ClipboardForm'
 import EmptyForm from './EmptyForm'
@@ -13,30 +12,28 @@ export default function SystemEventActionForm({
   selectedElement,
   selectedElementId
 }: Props) {
-  return useMemo(() => {
-    switch (selectedElement.data.type) {
-      case 'Open':
+  switch (selectedElement.data.type) {
+    case 'Open':
+      return (
+        <OpenEventForm
+          selectedElementId={selectedElementId}
+          selectedElement={selectedElement}
+        />
+      )
+    case 'Volume':
+      return <EmptyForm />
+    case 'Clipboard':
+      if (selectedElement.data.action.type === 'PasteUserDefinedString') {
         return (
-          <OpenEventForm
+          <ClipboardForm
             selectedElementId={selectedElementId}
             selectedElement={selectedElement}
           />
         )
-      case 'Volume':
+      } else {
         return <EmptyForm />
-      case 'Clipboard':
-        if (selectedElement.data.action.type === 'PasteUserDefinedString') {
-          return (
-            <ClipboardForm
-              selectedElementId={selectedElementId}
-              selectedElement={selectedElement}
-            />
-          )
-        } else {
-          return <EmptyForm />
-        }
-      default:
-        return <EmptyForm />
-    }
-  }, [selectedElement, selectedElementId])
+      }
+    default:
+      return <EmptyForm />
+  }
 }
